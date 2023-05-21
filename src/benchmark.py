547c4228bbf562055d7975f1e5595c03aa3b5bf4
lkotipal <leokotipalo@gmail.com>
2023-05-21 21:29:30 +0300

    New benchmark

--- src/benchmark.py
+++ src/benchmark.py
@@ -6,6 +6,7 @@ from sys import stderr
 from time import time
 
 # Randomize numeric values in the network
+# Takes as input dataframe of nodes and numpy rng object
 def setup(nodes, rng):
     nodes['Local Value'] = rng.uniform(high=10, size=nodes.shape[0])
     nodes['Trade Power'] = rng.uniform(high=1000, size=nodes.shape[0])
@@ -20,22 +21,29 @@ def setup(nodes, rng):
     nodes['Collecting Power'] = nodes['Trade Power'] * nodes['Collecting Power']
     nodes['Transfer Power'] = nodes['Trade Power'] * nodes['Transfer Power']
 
-def main():
-    print(np.__version__, file=stderr)
-    print(pd.__version__, file=stderr)
-    nodes = read_nodes()
+# Simulates the network multiple times in order to get an average time
+# Takes as input dataframe of nodes and number of times to repeat
+# Returns array of times
+def simulate(nodes, n):
     rng = np.random.default_rng(1444)
-    n = 1000
     t = np.zeros(n)
     for i in range(n):
         setup(nodes, rng)
         t0 = time()
         calculate_value(nodes)['My Value'].sum()
         t[i] = time() - t0
+    return t
+
+def main():
+    print(np.__version__, file=stderr)
+    print(pd.__version__, file=stderr)
+    nodes = read_nodes()
+    n = 1000
+    t = simulate(nodes, n)
+    # Standard error of mean
     mu = t.mean()
     sigma = t.std(ddof=1)
-    # Standard error of mean
-    print(f't = {mu} += {sigma / sqrt(n)}')
+    print(f't = {mu} +- {sigma / sqrt(n)}')
 
 if __name__ == '__main__':
     main()
